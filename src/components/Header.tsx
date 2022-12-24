f2b9c862d82eb284a0bc9ad5220de361d73c85dc
FelipeMDantas <felipemdantas@terra.com.br>
2022-12-23 23:43:43 -0300

    Header component stylized.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,77 @@
-import React from "react";
+import { EuiHeader, EuiText, EuiTextColor } from "@elastic/eui";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useAppSelector } from "../app/hooks";
 
 const Header = () => {
-  return <div>Header</div>;
+  const navigate = useNavigate();
+  const location = useLocation();
+  const username = useAppSelector((zoom) => zoom.auth.userInfo?.name);
+  const dispatch = useDispatch();
+
+  const [breadCrumbs, setBreadCrumbs] = useState([{ text: "Dashboard" }]);
+  const [isResponsive, setIsResponsive] = useState(false);
+
+  const section = [
+    {
+      items: [
+        <Link to="/">
+          <EuiText>
+            <h2 style={{ padding: "0 1vw" }}>
+              <EuiTextColor color="#0b5cff">Zoom</EuiTextColor>
+            </h2>
+          </EuiText>
+        </Link>,
+      ],
+    },
+  ];
+
+  const responsiveSection = [
+    {
+      items: [
+        <Link to="/">
+          <EuiText>
+            <h2 style={{ padding: "0 1vw" }}>
+              <EuiTextColor color="#0b5cff">Zoom</EuiTextColor>
+            </h2>
+          </EuiText>
+        </Link>,
+      ],
+    },
+    {
+      items: [
+        <>
+          {username ? (
+            <EuiText>
+              <h3>
+                <EuiTextColor color="white">Hello, </EuiTextColor>
+                <EuiTextColor color="#0b5cff">{username}</EuiTextColor>
+              </h3>
+            </EuiText>
+          ) : null}
+        </>,
+      ],
+    },
+  ];
+
+  useEffect(() => {
+    if (window.innerWidth < 480) setIsResponsive(true);
+  }, []);
+
+  return (
+    <>
+      <EuiHeader
+        style={{ minHeight: "8vh" }}
+        theme="dark"
+        sections={isResponsive ? responsiveSection : section}
+      />
+      <EuiHeader
+        style={{ minHeight: "8vh" }}
+        sections={[{ breadcrumbs: breadCrumbs }]}
+      />
+    </>
+  );
 };
 
 export default Header;
