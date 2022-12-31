8cd3f08e86dea68c73640ef618612ad2e674d5ab
FelipeMDantas <felipemdantas@terra.com.br>
2022-12-31 17:03:38 -0300

    MyMeetings page coded  and .env file added.

--- src/pages/MyMeetings.tsx
+++ src/pages/MyMeetings.tsx
@@ -1,10 +1,87 @@
+import { getDocs, query, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import { useAppSelector } from "../app/hooks";
+import useAuth from "../hooks/useAuth";
+import { meetingsRef } from "../utils/FirebaseConfig";
+import { MeetingType } from "../utils/Types";
+import Header from "../components/Header";
+import {
+  EuiBasicTable,
+  EuiButtonIcon,
+  EuiCopy,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiPanel,
+} from "@elastic/eui";
 
 const MyMeetings = () => {
-  const [meetings, setMeetings] = useState<Array<any>>([]);
+  useAuth();
+  const [meetings, setMeetings] = useState<any>([]);
+  const userInfo = useAppSelector((zoom) => zoom.auth.userInfo);
 
-  useEffect(() => {});
-  return <div>MyMeetings</div>;
+  useEffect(() => {
+    if (userInfo) {
+      const getMyMeetings = async () => {
+        const firestoreQuery = query(
+          meetingsRef,
+          where("createdBy", "==", userInfo?.uid)
+        );
+        const fetchedMeetings = await getDocs(firestoreQuery);
+
+        if (fetchedMeetings.docs.length) {
+          const myMeetings: Array<MeetingType> = [];
+          fetchedMeetings.forEach((meeting) => {
+            myMeetings.push({
+              docId: meeting.id,
+              ...(meeting.data() as MeetingType),
+            });
+          });
+          setMeetings(myMeetings);
+        }
+      };
+      getMyMeetings();
+    }
+  }, [userInfo]);
+
+  const columns = [
+    { field: "meetingName", name: "Meeting Name" },
+    { field: "meetingType", name: "Meeting Type" },
+    { field: "meetingDate", name: "Meeting Date" },
+    { field: "", name: "Status" },
+    { field: "", name: "Edit" },
+    {
+      field: "meetingId",
+      name: "Copy Link",
+      render: (meetingId: string) => {
+        return (
+          <EuiCopy
+            textToCopy={`${process.env.REACT_APP_HOST}/join/${meetingId}`}
+          >
+            {(copy: any) => (
+              <EuiButtonIcon
+                iconType="copy"
+                onClick={copy}
+                display="base"
+                aria-label="Meeting-copy"
+              />
+            )}
+          </EuiCopy>
+        );
+      },
+    },
+  ];
+  return (
+    <div style={{ display: "flex", height: "100vh", flexDirection: "column" }}>
+      <Header />
+      <EuiFlexGroup justifyContent="center" style={{ margin: "1rem" }}>
+        <EuiFlexItem>
+          <EuiPanel>
+            <EuiBasicTable items={meetings} columns={columns} />
+          </EuiPanel>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </div>
+  );
 };
 
 export default MyMeetings;
